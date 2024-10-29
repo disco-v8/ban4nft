5767ca8f4ea220b07666938cb30736ff484fc451
T.Kabu <kabu@fvg-on.net>
2024-10-29 14:57:59 +0900

    Update ban4nftd_ban.php
    Notification emails were not sent when iss_flag was 0.

--- ban4nftd_ban.php
+++ ban4nftd_ban.php
@@ -195,7 +195,16 @@ function ban4nft_ban($TARGET_CONF)
                     // -----------------------------
                     // 情報共有フラグがON(=1)なら、かつ情報共有サーバーからのデータでなければ
                     // -----------------------------
-                    if (isset($TARGET_CONF['iss_flag']) && $TARGET_CONF['iss_flag'] == 1 && $TARGET_CONF['target_service'] != 'iss-list')
+                    if (isset($TARGET_CONF['iss_flag'])
+                    {
+                        if ($TARGET_CONF['iss_flag'] == 1 && $TARGET_CONF['target_service'] != 'iss-list')
+                        {
+                            // BANした旨をメールで通知
+                            ban4nft_banmailsend($TARGET_CONF);
+                        }
+                    }
+                    // 情報共有フラグがON(=1)ではないなら
+                    else
                     {
                         // BANした旨をメールで通知
                         ban4nft_banmailsend($TARGET_CONF);
@@ -242,7 +251,16 @@ function ban4nft_ban($TARGET_CONF)
                     // -----------------------------
                     // 情報共有フラグがON(=1)なら、かつ情報共有サーバーからのデータでなければ
                     // -----------------------------
-                    if (isset($TARGET_CONF['iss_flag']) && $TARGET_CONF['iss_flag'] == 1 && $TARGET_CONF['target_service'] != 'iss-list')
+                    if (isset($TARGET_CONF['iss_flag'])
+                    {
+                        if ($TARGET_CONF['iss_flag'] == 1 && $TARGET_CONF['target_service'] != 'iss-list')
+                        {
+                            // BANした旨をメールで通知
+                            ban4nft_banmailsend($TARGET_CONF);
+                        }
+                    }
+                    // 情報共有フラグがON(=1)ではないなら
+                    else
                     {
                         // BANした旨をメールで通知
                         ban4nft_banmailsend($TARGET_CONF);
