b4b644012b674c112e1e45baa253a4fc156620b0
T.Kabu <kabu@fvg-on.net>
2025-03-05 10:56:42 +0900

    Update ban4nftd_unban.php
    エラーだったらFALSEにしてDBのリセット判定をするように処理の流れを変更

--- ban4nftd_unban.php
+++ ban4nftd_unban.php
@@ -124,7 +124,8 @@ function ban4nft_unban($TARGET_CONF)
     catch (PDOException $PDO_E) {
         // エラーの旨メッセージを設定
         $TARGET_CONF['log_msg'] .= date("Y-m-d H:i:s", local_time())." ban4nft[".getmypid()."]: WARN PDOException:".$PDO_E->getMessage()." on ".__FILE__.":".__LINE__."\n";
-        $RESULT = TRUE;
+        // TRUEにしていたが、エラーだったらFALSEにしてDBのリセット判定をするように処理の流れを変更 2025.03.05 T.Kabu
+        $RESULT = FALSE;
     }
     // 削除できなかったら
     if ($RESULT === FALSE)
